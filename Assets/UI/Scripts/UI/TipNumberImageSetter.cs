2a2487e05a4df571496b866a2a8b5db15285e45f
liulili <liulili@shinezone.com>
2017-09-21 15:35:54 +0800

    新功能 --- 增加tips显示功能；增加数字以tips方式显示

---
+++ Assets/UI/Scripts/UI/TipNumberImageSetter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TipNumberImageSetter : MonoBehaviour {
+
+    public List<Sprite> positiveImages;
+    public List<Sprite> negativeImages;
+    public Sprite positive;
+    public Sprite negative;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
