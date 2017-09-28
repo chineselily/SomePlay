d2fa9b97b1edb0a19b96b6b3eada050b7d48b93b
liulili <liulili@shinezone.com>
2017-09-28 11:40:39 +0800

    新功能 ---  无人机 --- 主界面（待完善）

---
+++ Assets/UI/Scripts/UI/DroneMainUI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TinyTeam.UI;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DroneMainUI : TTUIPage
+{
+
+    public DroneMainUI() : base(UIType.Normal, UIMode.DoNothing, UICollider.None) {
+        uiPath = "UIPrefabs/DroneMainUI";
+    }
+    public override void Awake(GameObject go)
+    {
+        transform.root.GetComponent<Canvas>().renderMode = RenderMode.ScreenSpaceOverlay;
+        CanvasScaler cs = transform.root.GetComponent<CanvasScaler>();
+        cs.uiScaleMode = CanvasScaler.ScaleMode.ScaleWithScreenSize;
+        cs.referenceResolution = new Vector2(2208f, 1242f);//new Vector2(1136f, 640f);
+        cs.screenMatchMode = CanvasScaler.ScreenMatchMode.MatchWidthOrHeight;//CanvasScaler.ScreenMatchMode.Expand;
+        cs.matchWidthOrHeight = 1;
+    }
+}
